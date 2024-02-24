5a19d991a0de3ee5364fa89e5a165903c09b9241
vincent31545 <vincentlau31545@gmail.com>
2024-02-23 22:51:47 -0500

    contributed a function to get the current voxel on the screen

--- STIB_Unity/Assets/Scripts/PhysicsManager/PhysicsManager.cs
+++ STIB_Unity/Assets/Scripts/PhysicsManager/PhysicsManager.cs
@@ -5,7 +5,31 @@ using UnityEngine;
 using UnityEngine.VFX;
 
 
-public class PhysicsManager: MonoBehaviour
-{
+public class PhysicsManager: MonoBehaviour {
+
     void Start() { }
+
+    public static Voxel GetVoxelHit(Vector3 rayOrigin, Vector3 rayDir, float rayLength, out Vector3Int normal) {
+        var norm = rayDir.normalized; 
+        int sectionCount = Mathf.CeilToInt(rayLength / 0.01f);
+
+        Voxel v; 
+        Vector3Int current, last = Vector3Int.zero; 
+
+        for (int i = 0; i < sectionCount; i++) {
+            current = WorldManager.GetGridPos(rayOrigin + norm * 0.01f * i); 
+            v = WorldManager.GetVoxel(current); 
+
+            if (v != null) { 
+                if (last == Vector3Int.zero)
+                    normal = Vector3Int.down; 
+                else
+                    normal = (last - v.position); 
+                return v; 
+            }
+            last = current; 
+        }
+        
+        normal = Vector3Int.zero; return null; 
+    }
 }
