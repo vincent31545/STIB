b058609d8a24991cec7b33d7453afbd658a65ee8
Daniel <nah@rpi.edu>
2024-03-12 17:22:24 -0400

    AND Block Logic

--- STIB_Unity/Assets/Scripts/VoxelRenderer/VoxelTypes/Voxel_AND.cs
+++ STIB_Unity/Assets/Scripts/VoxelRenderer/VoxelTypes/Voxel_AND.cs
@@ -7,4 +7,30 @@ public class Voxel_AND : Voxel {
     public Voxel_AND(VOXEL_TYPE _type, Vector3Int _pos) : base(_type, _pos) {
         Debug.Log("AND");
     }
+
+    public override void UpdateSignal() {
+        bool[] inSignal = new bool[6];
+        int counter = 0;
+        for (int i = 0; i < 6; i++) {
+            if (adjacent[i] == null) continue;
+            // Invert signal position
+            // ie if block on the right outgoing then block on left incoming
+            if (i%2 == 1)
+                inSignal[i] = adjacent[i].signals[i-1];
+                if (adjacent[i].signals[i-1]) counter++;
+            else 
+                inSignal[i] = adjacent[i].signals[i];
+                if (adjacent[i].signals[i]) counter++;
+        }
+        if (counter < 2) {
+            for (int i = 0; i < 6; i++) {
+                this.signals[i] = false;
+            }
+        }  
+        else {
+            for (int i = 0; i < 6; i++) {
+                this.signals[i] = !inSignal[i];
+            }
+        }
+    }
 }
