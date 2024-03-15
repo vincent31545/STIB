b3b467fda8a31368d7efac55b7794051be0a0366
Daniel <nah@rpi.edu>
2024-03-15 17:15:05 -0400

    logic children

--- STIB_Unity/Assets/Scripts/VoxelRenderer/VoxelTypes/Voxel_WIRE.cs
+++ STIB_Unity/Assets/Scripts/VoxelRenderer/VoxelTypes/Voxel_WIRE.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Voxel_AND : Voxel {
+public class Voxel_WIRE: Voxel {
 
-    public Voxel_AND(VOXEL_TYPE _type, Vector3Int _pos, Voxel[] _adjacent) : base(_type, _pos, _adjacent) {
-        Debug.Log("CREATION: AND block");
+    public Voxel_WIRE(VOXEL_TYPE _type, Vector3Int _pos, Voxel[] _adjacent) : base(_type, _pos, _adjacent) {
+        Debug.Log("CREATION: WIRE block");
+        SendSignal(false);
     }
   
-    public override Color GetVoxelColor() => Color.black;
-
     public override void UpdateSignal() {
-        int counter = 0;
         
+        // Checking all inputs
         for (int i = 0; i < 6; i++) {
             if (adjacent[i] == null || i == forward) continue;
             // Invert signal position
             // ie if block on the right outgoing then block on left incoming 
             if (adjacent[i].signals[ (i%2 == 1) ? i : (i-1)]) {
-                counter++;
+                SendSignal(true);
+                return;
             }
         }
-        
-        // Set output 
-        if (counter < 2) SendSignal(false);
-        else SendSignal(true);
+        SendSignal(false);
     }
+
+    public override Color GetVoxelColor() => Color.black;
 }
+
