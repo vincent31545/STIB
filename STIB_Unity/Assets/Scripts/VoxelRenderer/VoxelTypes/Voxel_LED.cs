b9d6c1464ab4bcceabb866b90145a583f5f35816
vincent31545 <vincentlau31545@gmail.com>
2024-04-05 16:36:03 -0400

    fixing led problem again

--- STIB_Unity/Assets/Scripts/VoxelRenderer/VoxelTypes/Voxel_LED.cs
+++ STIB_Unity/Assets/Scripts/VoxelRenderer/VoxelTypes/Voxel_LED.cs
@@ -17,7 +17,7 @@ public class Voxel_LED: Voxel {
 
     public override void UpdateSignal() {
        for (int i = 0; i < 6; i++) {
-            if (adjacent[i] == null || i == forward) continue;
+            if (adjacent[i] == null) continue;
             // Invert signal position
             // ie if block on the right outgoing then block on left incoming 
             if (adjacent[i].signals[ (i%2 == 1) ? (i-1) : (i+1)]) {
