dbd64195e16a89289d5a29288bd8715209cb3d7d
Reid Taylor <reidhtaylor@icloud.com>
2024-04-05 16:34:16 -0400

    Fixed lighting on LED

--- STIB_Unity/Assets/Scripts/VoxelRenderer/VoxelTypes/Voxel_SEND.cs
+++ STIB_Unity/Assets/Scripts/VoxelRenderer/VoxelTypes/Voxel_SEND.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Voxel_SEND: Voxel {
 
     public Voxel_SEND(VOXEL_TYPE _type, Vector3Int _pos, Voxel[] _adjacent) : base(_type, _pos, _adjacent) {
-        Debug.Log("Created SEND BLOCK");
+
     }
 
     public override void Initialize() {
